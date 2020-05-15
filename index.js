d882ff26adbb0154e62928ed01e4d7e48276ce69
Abhay Jirati <abhi.jrt12@gmail.com>
2020-05-15 13:11:29 +0530

    First Commit : Start Project setting directory Structure and express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error on running port : ${port}`);
+        return;
+    }
+    console.log(`Start server on port : ${port}`);
+});
