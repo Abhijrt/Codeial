f62493adc95aa19eb589abe1e215cfdbdcea4741
Abhay Jirati <abhi.jrt12@gmail.com>
2020-05-19 21:43:39 +0530

    add views and ejs and static and layout and connect to DataBase

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.use(express.static('./assets'));
 app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port, function(err){
     if(err){
