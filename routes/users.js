34863116c858e0f9607cfdf1256885a32304907d
Abhay Jirati <abhi.jrt12@gmail.com>
2020-05-21 14:02:02 +0530

    adding login with passport js

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const usersController = require('../controllers/users_controller');
 router.get('/profile',usersController.profile);
 router.get('/sign_up',usersController.signUp);
 router.get('/sign_in',usersController.signIn);
 router.post('/create',usersController.create);
-router.post('/create-session',usersController.createSession);
+router.post('/create-session',passport.authenticate(
+    'local' ,{failureRedirect : '/users/sign_in'}
+),usersController.createSession);
 module.exports = router;
 
