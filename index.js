175b3c7eca012ea26e761c1a026d6d4d9bc6a783
Abhay Jirati <abhi.jrt12@gmail.com>
2020-05-21 16:14:59 +0530

    setting up mongo store for session cookies to store in database

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
 app.use(express.urlencoded());
 app.use(expressLayouts);
 app.use(cookieParser());
@@ -20,11 +21,17 @@ app.use(session({
     resave : false,
     cookie : {
         maxAge : (1000 * 60 * 100)
+    },
+    store : new MongoStore({
+        mongooseConnection : db,
+        autoRemove : 'disabled'
+    },function(err){
+        console.log(err || 'connect-mongodb setup ok');
     }
+    )
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(passport.setAuthenticatedUser);
 app.use('/',require('./routes'));
 app.listen(port, function(err){
