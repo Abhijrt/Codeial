7436fff347c2228d9a43be9962293cf1d7ebee54
Abhay Jirati <abhi.jrt12@gmail.com>
2020-05-27 18:24:34 +0530

    adding the file structure of api and api code

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,55 @@
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req,res){
+
+    try{
+        let posts = await Post.find({}).populate('user').populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+        return res.json(200,{
+            message : "List of Posts",
+            posts : posts
+        });
+    }catch(err){
+        return res.json(200,{
+            message : "error in index"
+        })
+    }
+}
+
+
+module.exports.destroy = async function (req, res) {
+    try {
+        let post = await Post.findById(req.params.id);
+        // if (post.user == req.user.id) {
+            post.remove();
+            await Comment.deleteMany(
+                { post: req.params.id });
+            // if (req.xhr) {
+            //     return res.status(200).json({
+            //         data: {
+            //             post_id: req.params.id
+            //         },
+            //         message: "Post deleted"
+            //     });
+            // }
+
+            // req.flash('success', 'Post and associated comments deleted!');
+
+            return res.json(5000,{
+                message : "Internal Server Error"
+            });
+        // }
+        // else {
+            // req.flash('error', err);
+            // return res.redirect('back');
+        // }
+    } catch (err) {
+        console.log(err);
+        return;
+    }
+}
