03cb5837e06516f4098123fcf70bb0f2d053a1f8
Abhay Jirati <abhi.jrt12@gmail.com>
2020-06-01 18:12:25 +0530

    adding the production mode and doing changes for the production

--- config/environment.js
+++ config/environment.js
@@ -24,8 +24,8 @@ const development = {
 const production = {
     name : 'production',
     asset_path : process.env.CODEIAL_ASSETS_PATH,
-    session_cookie_key : 'blahsomething',
-    db : 'codeial-development',
+    session_cookie_key : process.env.CODEIAL_SESSION_COOKIE,
+    db : process.env.CODEIAL_DB,
     smtp : {
         service : 'gmail',
         host :'smtp.gmail.com',
@@ -39,7 +39,10 @@ const production = {
     Google_client_ID : '492562497322-0q3vfbu6h8nt2o254qfceldu21qlrabi.apps.googleusercontent.com',
     Google_client_Secret : '3bmxxgkOMFU7_gWV64B1pbmJ',
     Google_callback_URL : 'http://localhost:8000/users/auth/google/callback',
-    Google_JWT_Secret : 'codeial',
+    Google_JWT_Secret : process.env.CODEIAL_JWT_SECRET,
 }
 
-module.exports = development;
+// module.exports = development;
+
+// for production mode or a developement
+module.exports = eval(production == undefined) ? development : eval(production);
